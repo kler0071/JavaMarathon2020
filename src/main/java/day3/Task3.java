75be4cf6f0e6bb41f324e48593c4f176bbc9c28f
Ilya <posinitskiy@gmail.com>
2020-09-23 19:39:04 +0300

    Add tasks solutions with notes day3

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int i = 0;
+        while (i < 5) {
+
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            double c = a / b;
+            System.out.println(c);
+            i++;
+        }
     }
 }
