24a1ebfcc0436f94aeea2b58049524ebf74850ad
Ilya <posinitskiy@gmail.com>
2020-09-25 17:12:42 +0300

    Add tasks solutions day5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,14 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
 
+        car.setYear(2005);
+        car.setColor("blue");
+        car.setModel("Mazda");
+
+        System.out.println(car.getYear());
+        System.out.println(car.getColor());
+        System.out.println(car.getModel());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -2,6 +2,8 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(1980, "grey", "Honda");
 
+        System.out.println(motorbike.getYear() + " " + motorbike.getColor() + " " + motorbike.getModel());
     }
 }
