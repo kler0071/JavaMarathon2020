a2e6aa7875fef434501c6a8878d70a3f9ec6851e
Ilya <posinitskiy@gmail.com>
2020-09-30 13:34:22 +0300

    Add tasks solutions day8

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,43 @@
+package day8;
+
+public class Airplane {
+    private String producer;
+    private int year;
+    private double length;
+    private double weight;
+    private double fuel;
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public Airplane(String producer, int year, double length, double weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + producer + ", Год выпуска: " + year + ", Длина: " + length + ", Вес: " + weight + ", Количество топлива в баке: " + fuel);
+    }
+
+    public void fillUp(double fuel) {
+        this.fuel += fuel;
+    }
+
+    @Override
+    public String toString() {
+        return "Изготовитель: " + producer + ", Год выпуска: " + year + ", Длина: " + length + ", Вес: " + weight + ", Количество топлива в баке: " + fuel;
+    }
+}
+
