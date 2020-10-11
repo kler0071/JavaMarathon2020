703cf55c8459280ea275e54643a491537a902592
Ilya <posinitskiy@gmail.com>
2020-10-11 16:48:26 +0300

    Adding solution of task day17

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,12 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece[] mass = {ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+                ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK};
+
+        for (int i = 0; i < mass.length; i++) {
+            System.out.print(mass[i].getName() + " ");
+        }
 
     }
 }
